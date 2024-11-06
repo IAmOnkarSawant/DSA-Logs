a8e7446630d1248165a718c6065f3dbf62e47678
Onkar Bharat Sawant <68294883+IAmOnkarSawant@users.noreply.github.com>
2024-11-06 09:16:09 +0530

    Time: 7 ms (24.46%) | Memory: 31.9 MB (31.38%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int count_set_bits(int num)
+    {
+        int cnt = 0;
+        while(num > 0)
+        {
+            cnt++;
+            num = num & num-1;
+        }
+        return cnt;
+    }
+    bool canSortArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> set_bits;
+        for(int num: nums)
+        {
+            set_bits.push_back(count_set_bits(num));
+            // cout<<set_bits.back()<<" ";
+        }
+
+        for(int i = 0; i < n-1; i++)
+        {
+            for(int j = 0; j < n-i-1; j++)
+            {
+                if(nums[j+1] < nums[j] && set_bits[j] == set_bits[j+1])
+                {
+                    swap(nums[j], nums[j+1]);
+                    swap(set_bits[j], set_bits[j+1]);
+                }
+            }
+        }
+        // cout<<"\n";
+        // for(int i = 0; i < n; i++)
+        // {
+        //     cout<<nums[i]<<" ";
+        // }
+        return is_sorted(nums.begin(), nums.end());
+    }
+};
