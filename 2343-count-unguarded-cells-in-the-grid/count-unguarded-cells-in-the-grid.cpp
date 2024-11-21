ab8f77c783231e5891f7bc00edd2c4d9c886542a
Onkar Bharat Sawant <68294883+IAmOnkarSawant@users.noreply.github.com>
2024-11-21 11:16:53 +0530

    Time: 2797 ms (5.16%) | Memory: 208.3 MB (29.41%) - LeetSync

---
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    /*
+        1 - unguarded cell
+        0 - blocked
+        -1 - wall
+        2 - guard
+    */
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<int>>pos(m, vector<int>(n, 1));
+        for(auto pair: walls)
+        {
+            int i = pair[0];
+            int j = pair[1];
+
+            pos[i][j] = -1;
+        }
+        for(auto g: guards)
+        {
+            int gi = g[0];
+            int gj = g[1];
+            pos[gi][gj] = 2;
+        }
+
+        for(int gi = 0; gi < m; gi++)
+        {
+            for(int gj = 0; gj < n; gj++)
+            {
+                if(pos[gi][gj] == 2)
+                {
+                     for(int i = gi-1; i >= 0 ; i--)
+                    {
+                        if(pos[i][gj] == -1 || pos[i][gj] == 2)
+                            break;
+                        pos[i][gj] = 0;
+                    }
+                    for(int i = gi+1; i < m ; i++)
+                    {
+                        if(pos[i][gj] == -1 || pos[i][gj] == 2)
+                            break;
+                        pos[i][gj] = 0;
+                    }
+                    for(int i = gj-1; i >= 0 ; i--)
+                    {
+                        if(pos[gi][i] == -1 || pos[gi][i] == 2)
+                            break;
+                        pos[gi][i] = 0;
+                    }
+                    for(int i = gj+1; i < n ; i++)
+                    {
+                        if(pos[gi][i] == -1 || pos[gi][i] == 2)
+                            break;
+                        pos[gi][i] = 0;
+                    }
+                }
+
+            }
+        }
+       
+        int cnt = 0;
+        for(auto vec: pos)
+        {
+            for(auto ele: vec)
+            {
+                cout<<ele<<" ";
+                if(ele == 1)
+                    cnt++;
+            }
+            cout<<"\n";
+        }
+        return cnt;
+    }
+};
